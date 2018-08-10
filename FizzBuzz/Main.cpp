56d8d9b504cab25c2b5051b5cf7d20a933f5448d
Richard Horton <richardhorton97@outlook.com>
2018-08-10 18:13:31 +0100

    Added comments

--- FizzBuzz/Main.cpp
+++ FizzBuzz/Main.cpp
@@ -6,19 +6,19 @@ int main()
 {
 	for (int i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)  // Prints FizzBuzz when number is divisible by both 3 and 5
 		{
 			std::cout << "FizzBuzz" << std::endl;
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0)  // Prints Fizz when number is divisible by 3
 		{
 			std::cout << "Fizz" << std::endl;
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0)  // Prints Buzz when number is divisible by 5
 		{
 			std::cout << "Buzz" << std::endl;
 		}
-		else
+		else  // Prints the number when it is not divisible by 3 or 5
 		{
 			std::cout << i << std::endl;
 		}
